5a3588dfe899fd73571379442a0020113c9388db
TritonSpear <90584343+TritonSpear@users.noreply.github.com>
2025-09-22 19:29:29 -0400

    Add files via upload

---
+++ digicompanion.py
@@ -0,0 +1,55 @@
+#digicompanion.py
+import time
+from datetime import datetime
+from tqdm import tqdm
+
+ascii_art = r"""
+⠄⠄⠄⠄⢠⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⣿⣿⣆⠄⠄⠄
+⠄⠄⣼⢀⣿⣿⣿⣿⣏⡏⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿⣿⡆⠄⠄
+⠄⠄⡟⣼⣿⣿⣿⣿⣿⠄⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⣿⣇⢻⣿⣿⣿⣿⠄⠄
+⠄⢰⠃⣿⣿⠿⣿⣿⣿⠄⠄⠄⠄⠄⠄⠙⠿⣿⣿⣿⣿⣿⠄⢿⣿⣿⣿⡄⠄
+⠄⢸⢠⣿⣿⣧⡙⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⡇⠸⣿⡿⣸⡇⠄
+⠄⠈⡆⣿⣿⣿⣿⣦⡙⠳⠄⠄⠄⠄⠄⠄⢀⣠⣤⣀⣈⠙⠃⠄⠿⢇⣿⡇⠄
+⠄⠄⡇⢿⣿⣿⣿⣿⡇⠄⠄⠄⠄⠄⣠⣶⣿⣿⣿⣿⣿⣿⣷⣆⡀⣼⣿⡇⠄
+⠄⠄⢹⡘⣿⣿⣿⢿⣷⡀⠄⢀⣴⣾⣟⠉⠉⠉⠉⣽⣿⣿⣿⣿⠇⢹⣿⠃⠄
+⠄⠄⠄⢷⡘⢿⣿⣎⢻⣷⠰⣿⣿⣿⣿⣦⣀⣀⣴⣿⣿⣿⠟⢫⡾⢸⡟⠄.
+⠄⠄⠄⠄⠻⣦⡙⠿⣧⠙⢷⠙⠻⠿⢿⡿⠿⠿⠛⠋⠉⠄⠂⠘⠁⠞⠄⠄⠄
+⠄⠄⠄⠄⠄⠈⠙⠑⣠⣤⣴⡖⠄⠿⣋⣉⣉⡁⠄⢾⣦⠄⠄⠄⠄⠄⠄⠄⠄
+"""
+print(ascii_art)
+print("Welcome to DigiCompanion!")
+(time.gmtime())
+print("Current time (UTC):", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+print("Please continue your work till the time is up.")
+
+tasks = r"""
+Tasks required: """
+tasks = input("What are we working on today?  ")
+
+
+
+start = input("Type yes to start the timer(type help to see other commands): ")
+
+if start == "yes":
+    for i in range(5):
+        print("Get to work!")
+        for i in tqdm(range(100)):
+            time.sleep(0.01)  # Simulating a task
+        
+        print("Time's up! Please take a break and stretch.")
+        for i in tqdm(range(100)):
+            time.sleep(0.01) 
+        print(next_level)
+print("How much progress have we made?     ")
+numbers = [int(n) for n in input('Enter percentage: ').split()]
+if start == "Show progress":
+    print(tasks, numbers)
+if start == "help":
+    print("I havnt done this yet")
+def loop():
+    next_level = input("Would you like to continue? yes/no: ") 
+    if next_level == "yes":
+        loop()
+    elif next_level == "no":
+        print("Goodbye!")
+        exit()
